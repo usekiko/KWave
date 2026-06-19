5c38719a3eadb8bae07d75637cd1325a5eb39831
usekiko <id4rp02s@gmail.com>
2026-06-19 16:17:47 +0200

    refactor: remove legacy floating markers and 3d text

--- [core]/kw_core/client/modules/events.lua
+++ [core]/kw_core/client/modules/events.lua
@@ -252,47 +252,7 @@ AddStateBagChangeHandler("group", "", function(bagName, _, value, _, _)
     KW.SetPlayerData("group", value)
 end)
 
-if not Config.CustomInventory then
-    KW.SecureNetEvent("kw:createPickup", function(pickupId, label, coords, itemType, name, components, tintIndex)
-        local function setObjectProperties(object)
-            SetEntityAsMissionEntity(object, true, false)
-            PlaceObjectOnGroundProperly(object)
-            FreezeEntityPosition(object, true)
-            SetEntityCollision(object, false, true)
 
-            pickups[pickupId] = {
-                obj = object,
-                label = label,
-                inRange = false,
-                coords = coords,
-            }
-        end
-
-        if itemType == "item_weapon" then
-            local weaponHash = joaat(name)
-            KW.Streaming.RequestWeaponAsset(weaponHash)
-            local pickupObject = CreateWeaponObject(weaponHash, 50, coords.x, coords.y, coords.z, true, 1.0, 0)
-            SetWeaponObjectTintIndex(pickupObject, tintIndex)
-
-            for _, v in ipairs(components) do
-                local component = KW.GetWeaponComponent(name, v)
-                if component then
-                    GiveWeaponComponentToWeaponObject(pickupObject, component.hash)
-                end
-            end
-
-            setObjectProperties(pickupObject)
-        else
-            KW.Game.SpawnLocalObject("prop_money_bag_01", coords, setObjectProperties)
-        end
-    end)
-
-    KW.SecureNetEvent("kw:createMissingPickups", function(missingPickups)
-        for pickupId, pickup in pairs(missingPickups) do
-            TriggerEvent("kw:createPickup", pickupId, pickup.label, vector3(pickup.coords.x, pickup.coords.y, pickup.coords.z - 1.0), pickup.type, pickup.name, pickup.components, pickup.tintIndex)
-        end
-    end)
-end
 
 KW.SecureNetEvent("kw:registerSuggestions", function(registeredCommands)
     for name, command in pairs(registeredCommands) do
@@ -302,14 +262,7 @@ KW.SecureNetEvent("kw:registerSuggestions", function(registeredCommands)
     end
 end)
 
-if not Config.CustomInventory then
-    KW.SecureNetEvent("kw:removePickup", function(pickupId)
-        if pickups[pickupId] and pickups[pickupId].obj then
-            KW.Game.DeleteObject(pickups[pickupId].obj)
-            pickups[pickupId] = nil
-        end
-    end)
-end
+
 
 function StartServerSyncLoops()
     if Config.CustomInventory then return end
@@ -369,49 +322,7 @@ function StartServerSyncLoops()
     end)
 end
 
-if not Config.CustomInventory then
-    CreateThread(function()
-        while true do
-            local Sleep = 1500
-            local playerCoords = GetEntityCoords(KW.PlayerData.ped)
-            local _, closestDistance = KW.Game.GetClosestPlayer(playerCoords)
 
-            for pickupId, pickup in pairs(pickups) do
-                local distance = #(playerCoords - pickup.coords)
-
-                if distance < 5 then
-                    Sleep = 0
-                    local label = pickup.label
-
-                    if distance < 1 then
-                        if IsControlJustReleased(0, 38) then
-                            if IsPedOnFoot(KW.PlayerData.ped) and (closestDistance == -1 or closestDistance > 3) and not pickup.inRange then
-                                pickup.inRange = true
-
-                                local dict, anim = "weapons@first_person@aim_rng@generic@projectile@sticky_bomb@", "plant_floor"
-                                KW.Streaming.RequestAnimDict(dict)
-                                TaskPlayAnim(KW.PlayerData.ped, dict, anim, 8.0, 1.0, 1000, 16, 0.0, false, false, false)
-                                RemoveAnimDict(dict)
-                                Wait(1000)
-
-                                TriggerServerEvent("kw:onPickup", pickupId)
-                                PlaySoundFrontend(-1, "PICK_UP", "HUD_FRONTEND_DEFAULT_SOUNDSET", false)
-                            end
-                        end
-
-                        label = ("%s~n~%s"):format(label, TranslateCap("threw_pickup_prompt"))
-                    end
-
-                    local textCoords = pickup.coords + vector3(0.0, 0.0, 0.25)
-                    KW.Game.Utils.DrawText3D(textCoords, label, 1.2, 1)
-                elseif pickup.inRange then
-                    pickup.inRange = false
-                end
-            end
-            Wait(Sleep)
-        end
-    end)
-end
 
 ----- Admin commands from kw_adminplus
 RegisterNetEvent("kw:tpm", function()
